5367389e7ab91af0ec5c7b6f673bb51eaa745681
roshnirifa <roshnirifa998@gmail.com>
2022-07-06 10:32:48 +0600

    jdoddle bakend set

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ function run() {
     let html = document.querySelector(".ide #html").value;
     let css = "<style>" + document.querySelector(".ide #css").value + "</style>";
     let js = document.querySelector(".ide #js").value;
+    let c = document.querySelector(".ide #c").value;
     let output = document.querySelector(".ide #output");
 
-    output.contentDocument.body.innerHTML = html + css;
-    output.contentWindow.eval(js)
+
+
+    // output.contentDocument.body.innerHTML = html + css + c;
+    // output.contentWindow.eval(js)
+    fetch("http://localhost:5000/api", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+            code: c
+        })
+    })
+        .then((res) => res.json()).then(data => {
+            output.value = (data?.body?.output);
+        })
 
     // console.log(html, css, js, output);
 
 }
-document.querySelector(".ide #html").addEventListener('keyup', run)
-document.querySelector(".ide #css").addEventListener('keyup', run)
-document.querySelector(".ide #js").addEventListener('keyup', run)
+// document.querySelector(".ide #html").addEventListener('keyup', run)
+// document.querySelector(".ide #css").addEventListener('keyup', run)
+// document.querySelector(".ide #js").addEventListener('keyup', run)
+// document.querySelector(".ide #c").addEventListener('keyup', run)
+document.querySelector(".ide #submit-btn").addEventListener('click', run);
+
